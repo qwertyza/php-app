922273f8b42569dfd7c0cf5889d167528afb239e
Oleg Kulagin <OKulagin@veeam.com>
2020-01-31 15:08:14 +0300

    version3

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ else
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>PHP Application - AWS Elastic Beanstalk</title>
     <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
+    <link rel="stylesheet" href="http//fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
     <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
     <link rel="shortcut icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
     <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
